d25583634bba8fa215f58cd4d525bad7435e438c
Silver1063 <brickbrickerson1063@gmail.com>
2024-03-31 13:42:35 -0700

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,43 +1,29 @@
-import { searchByKeyword } from './api.js';
-import { Command } from 'commander';
+import { search } from "./app.js";
+import { Command } from "commander";
 
 // Create a new instance of Command
 const program = new Command();
+const options = program.opts();
 
 // Define the program description and version
 program
-  .version('1.0.0')
-  .description('CLI tool for searching Launch Library 2 API');
+  .version("1.0.0")
+  .description("CLI tool for searching Launch Library 2 API");
+
+// Add a cache option for searches
+program.option("-c, --cache", "Use local cache to make searches, default off");
 
 // Add a search command to search based on a keyword
 program
-  .command('search <keyword>')
-  .description('search based on your selected API using a keyword')
+  .command("search <keyword>")
+  .description("Search based on your selected API using a keyword")
   .action(async (keyword) => {
     try {
-      const searchResults = await searchByKeyword(keyword);
-      console.log('Search results:');
-      displayResults(searchResults);
+      await search(keyword, options.cache);
     } catch (error) {
-      console.error('Error:', error.message);
+      console.error("Error:", error.message);
     }
   });
 
-
 // Parse command-line arguments
 program.parse(process.argv);
-
-// Function to display search results
-function displayResults(results) {
-  for (const category in results) {
-    if (results.hasOwnProperty(category)) {
-      console.log(`--- ${category.charAt(0).toUpperCase() + category.slice(1)} ---`);
-      console.log(`Count: ${results[category].count}`);
-      console.log('Results:');
-      results[category].results.forEach(result => {
-        console.log(JSON.stringify(result, null, 2));
-      });
-      console.log('\n');
-    }
-  }
-}
